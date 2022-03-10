76167cc0243778ff91bbf976f88f127aa24e7d2f
FranciscoMalstim <franciscosantos2501@gmail.com>
2022-03-10 11:55:21 -0300

    add the graphics

---
+++ src/dummyData.js
@@ -0,0 +1,48 @@
+export const userData = [
+    {
+      name: 'Jan',
+      'Active User': 4000,
+    },
+    {
+      name: 'Fev',
+      'Active User': 3000,
+    },
+    {
+      name: 'Mar',
+      'Active User': 2000,
+    },
+    {
+      name: 'Abr',
+      'Active User': 2780,
+    },
+    {
+      name: 'Mai',
+      'Active User': 1890,
+    },
+    {
+      name: 'Jun',
+      'Active User': 2390,
+    },
+    {
+      name: 'Jul',
+      'Active User': 3490,
+    },{
+      name: 'Ago',
+      'Active User': 3490,
+    },{
+      name: 'Set',
+      'Active User': 4490,
+    },
+    {
+      name: 'Out',
+      'Active User': 5690,
+    },
+    {
+      name: 'Nov',
+      'Active User': 8790,
+    },
+    {
+      name: 'Dez',
+      'Active User': 12340,
+    },
+  ];
